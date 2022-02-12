80b261bf0692a17d719b345f1d4c4fe3f5034d74
LearnCodeWithH <68248525+LearnCodeWithH@users.noreply.github.com>
2022-02-11 23:05:48 -0800

    Why you no relative?

--- lib/Graphics.jsx
+++ lib/Graphics.jsx
@@ -7,8 +7,10 @@ function resizeItemByReplace(item, width_height) {
 }
 
 function resizeItemByFrameDimensions(item, width_height) {
+    // Reframe will scale opposing corners by ppi
     item.reframe(
-        CoordinateSpaces.INNER_COORDINATES,
+        // CoordinateSpaces.INNER_COORDINATES,
+        CoordinateSpaces.PAGE_COORDINATES,
         [[0,0],width_height]
     );
     item.fit(FitOptions.FRAME_TO_CONTENT);
@@ -26,15 +28,49 @@ function reframeToPageCoordPositionByULCorner(item, ul_corner_pos, width_height)
 * @param image  
 */
 function getPixels(image){
+    return getPixelsAtPpi(image, image.effectivePpi);
+}
 
-    var rx = image.effectivePpi[0];
-    var ry = image.effectivePpi[1];
+// Sourced from https://community.adobe.com/t5/indesign-discussions/having-trouble-extracting-the-pixel-dimensions-of-an-image-link-in-indesign/m-p/10952544#M177184
+/**
+* returns the pixel dimensions of an image at a specified ppi
+* @param image  
+* @param atPpi  
+*/
+function getPixelsAtPpi(image, atPpi) {
+    var rx = atPpi[0];
+    var ry = atPpi[1];
     var w = image.geometricBounds[3] - image.geometricBounds[1]; // x2-x1
     var h = image.geometricBounds[2] - image.geometricBounds[0]; // y2-y1
     var hpd = Math.round(w * rx);
     var vpd = Math.round(h * ry);
 
     return [hpd, vpd];
+}
+
+function normalizePixels(width_height, fromPpi) {
+    return normalizePixelsToPpi(width_height, fromPpi, [72.0, 72.0]);
+}
+
+function normalizePixelsToPpi(width_height, fromPpi, toPpi) {
+    // 72 desired effective ppi
+    // 102 current effective ppi
+    // 300 actual ppi
+    // 182 dimension - width
+    // 128 pixels
+    // ppi * pixels = dimen
+    // ppi * 128 = 182
+    return [
+        width_height[0] * (toPpi[0]/fromPpi[0]),
+        width_height[1] * (toPpi[1]/fromPpi[1])
+    ];
+}
+
+function getPageDimensions(page) {
+    var w = page.bounds[3] - page.bounds[1]; // x2-x1
+    var h = page.bounds[2] - page.bounds[0]; // y2-y1
+
+    return [w, h];
 }
 
 function usingViewPreferences(preferenceProperties, func) {
@@ -48,13 +84,17 @@ function usingViewPreferences(preferenceProperties, func) {
 }
 
 function findPixelDimensionsOfItem(item) {
+    return findPixelDimensionsOfItemAtPpi(item, item.effectivePpi)
+}
+
+function findPixelDimensionsOfItemAtPpi(item, atPpi) {
     return usingViewPreferences(
         {horizontalMeasurementUnits:MeasurementUnits.INCHES, verticalMeasurementUnits:MeasurementUnits.INCHES},
         function() {
             if ("Photoshop" === item.imageTypeName || 
             "JPEG" === item.imageTypeName ||
             "PNG" === item.imageTypeName) { // TODO: Can this be a check on the needed effectivePpi and geometricBounds?
-                return getPixels(item)
+                return getPixelsAtPpi(item, atPpi)
             }
             else if (undefined !== item.imageTypeName) {
                 alert("Unsupported graphic type '" + item.imageTypeName + "' was the first graphic in layer.");
