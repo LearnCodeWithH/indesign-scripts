b2d56b272148f2abaa5250745872fd4967a22aa9
LearnCodeWithH <68248525+LearnCodeWithH@users.noreply.github.com>
2025-04-20 20:51:46 -0700

    Foundation for PS script side

--- lib/bridgetalk/BuildPsdFromPdfPSEntrypoint.jsx
+++ lib/bridgetalk/BuildPsdFromPdfPSEntrypoint.jsx
@@ -6,95 +6,76 @@
 
 // Utf-8 test: 漢字
 
-function createTextLayersFromData(import_pdf_options, color_profile, layer_infos, user_export_folder) {
+function createTextLayersFromData(import_pdf_options, color_profile, page_info_by_page_num, user_export_folder) {
     var open_document_mode = colorModeToOpenDocumentMode(import_pdf_options.color_mode);
     var pdf_open_options = configurePdfOpenOptions(open_document_mode, import_pdf_options.dpi_res, import_pdf_options.anti_alias);
     pdf_open_options.page = 1; // All pdfs only have one page here.
 
-    // Open a page of the pdf to photoshop. Note the 3rd parameter is for smart objects. Needs to be false here or the TRIMBOX won’t work
-    var working_doc = app.open(open_pdf_file, pdf_open_options, false);
-    
-    // Now textData is directly the array of frames
-    for (var i = 0; i < textData.length; i++) {
-        var frameData = textData[i];
-        var textLayer = doc.artLayers.add();
-        textLayer.kind = LayerKind.TEXT;
-        
-        // Position the text layer
-        var textItem = textLayer.textItem;
-        textItem.position = [frameData.bounds.x, frameData.bounds.y];
-        
-        // Handle multiple style runs
-        var combinedText = "";
-        for (var j = 0; j < frameData.styleRuns.length; j++) {
-            var run = frameData.styleRuns[j];
-            combinedText += run.text;
-        }
-        
-        // Set initial text content
-        textItem.contents = combinedText;
-        
-        // Apply text frame properties
-        if (frameData.frameRotation) {
-            textLayer.rotate(frameData.frameRotation);
-        }
-        
-        // Set frame dimensions
-        textItem.width = frameData.bounds.width;
-        textItem.height = frameData.bounds.height;
-        
-        // Name the layer
-        textLayer.name = "Text Frame " + (i + 1);
-        
-        // Apply style runs
-        try {
-            var runs = frameData.styleRuns;
-            if (runs.length > 0) {
-                // Apply first run's properties to whole layer as base
-                var baseRun = runs[0];
-                textItem.font = baseRun.fontFamily;
-                textItem.size = baseRun.pointSize;
-                textItem.tracking = baseRun.tracking;
-                
-                // If we have multiple runs, we need to apply character styles
-                if (runs.length > 1) {
-                    var charStyles = textItem.characterStyles;
-                    var runStart = 0;
-                    
-                    for (var k = 0; k < runs.length; k++) {
-                        var run = runs[k];
-                        var runLength = run.text.length;
-                        
-                        if (runLength > 0) {
-                            var charStyle = charStyles.add();
-                            charStyle.range = [runStart, runStart + runLength];
-                            
-                            try {
-                                charStyle.font = run.fontFamily;
-                                charStyle.size = run.pointSize;
-                                charStyle.tracking = run.tracking;
-                            } catch(e) {}
-                            
-                            runStart += runLength;
-                        }
-                    }
+    usingFolder(new Folder(user_export_folder + "/EditablePSDs"), function(folder) {
+        for (var pageNum in page_info_by_page_num) {
+            var pageInfo = page_info_by_page_num[pageNum];
+            var psdDocument = openAndPreparePsdFromPdf(pageInfo.pdfFilePath, pdf_open_options, pageInfo.firstContentLayer);
+
+            for (var i = 0; i < pageInfo.layerInfoOrdered.length; i++) {
+                var layerInfo = pageInfo.layerInfoOrdered[i];
+                if (layerInfo.type === "text") {
+                    createTextLayerGroupFromInfo(psdDocument, pdf_open_options, layerInfo);
+                } else if (layerInfo.type === "content") {
+                    createContentLayerFromInfo(psdDocument, layerInfo);
+                } else {
+                    throw new Error("Unknown layer type: " + layerInfo.type);
                 }
             }
-        } catch(e) {
-            // Handle any errors in text styling
-            throw new Error("Error applying text styles: " + e.message);
+
+            // Save the PSD document
+            var pageNumPadded = zeroPad(pageNum, 4);
+            var psdFile = new File(folder + "/" + "Page-" + pageNumPadded + ".psd");
+            savePSD(psdDocument, psdFile);
         }
-    }
+    });
 }
 
-function createContentLayerFromInfo() {
-
+function savePSD(psdDocument, psdFile) {
+    var psSaveOptions = new PhotoshopSaveOptions();
+    psSaveOptions.embedColorProfile = true;
+    psSaveOptions.layers = true;
+    psSaveOptions.alphaChannels = true;
+    psdDocument.saveAs(psdFile, psSaveOptions, false);
+    psdDocument.close(SaveOptions.DONOTSAVECHANGES);
 }
 
-function createTextLayerGroupFromInfo() {
-    
+function openAndPreparePsdFromPdf(pdfOpenOptions, contentLayerInfo) {
+    var pdfFile = new File(contentLayerInfo.pdfFullFilePath);
+    // Open a page of the pdf to photoshop. Note the 3rd parameter is for smart objects. Needs to be false here or the TRIMBOX won’t work
+    var workingDoc = app.open(pdfFile, pdfOpenOptions, false);
+
+    // Unlock the layer and clear the layer entirely.
+    workingDoc.backgroundLayer.allLocked = false;
+    workingDoc.backgroundLayer.clear();
+
+    return workingDoc;
 }
 
-function createTextLayerWithStyleRuns() {
+function createContentLayerFromInfo(psdDocument, pdfOpenOptions, contentLayerInfo) {
+    var pdfFile = new File(contentLayerInfo.pdfFullFilePath);
+    // Open a page of the pdf to photoshop. Note the 3rd parameter is for smart objects. Needs to be false here or the TRIMBOX won’t work
+    var contentDoc = app.open(pdfFile, pdfOpenOptions, false);
 
+    // Unlock the layer for copy
+    contentDoc.backgroundLayer.allLocked = false;
+
+    // Copy the background layer into a new layer in psdDocument and send to 1 above back in layer ordering.
+}
+
+function createTextLayerGroupFromInfo(psdDocument, textLayerInfo) {
+    var textData = textLayerInfo.textData;
+
+    // Make a layer group for the text layers
+    // Send the layer group to 1 above back in layer ordering.
+    // Iterate through the text data and invoke createTextLayerWithStyleRuns for each entry.
+}
+
+function createTextLayerWithStyleRuns(psdDocument, textGroup, textDataEntry) {
+    // Create a new text layer in the text group
+    // Use the values of the textDataEntry to position, rotate, set style runs, etc for the text layer.
 }
